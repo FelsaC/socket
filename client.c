b32ede7bf48694dbd255a5915139da1cb7e23516
Alisson <alissondosantos374@gmail.com>
2026-05-30 19:47:15 -0300

    adiciona 'chat', comando para sair e execucao de comandos pelo clietne

--- client.c
+++ client.c
@@ -1,49 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include <arpa/inet.h> 
 
-#define PORTA 8080
-#define BUFFER_SIZE 1024
+#define PORTA 8080 
+#define BUFFER_SIZE 1024 
 
-int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    char *mensagem = "Ola Servidor, sou o Cliente!";
-    char buffer[BUFFER_SIZE] = {0};
+int main() { 
+    int sock = 0; 
+    struct sockaddr_in serv_addr; 
+    char mensagem[BUFFER_SIZE] = "Ola Servidor, sou o Cliente!"; 
+    char buffer[BUFFER_SIZE] = {0}; 
 
-    // 1. Criando o socket do cliente
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Erro na criacao do socket \n");
-        return -1;
+    // 1. Criando o socket do cliente 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+      printf("\n Erro na criacao do socket \n"); 
+      return -1; 
+    } 
+
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORTA); 
+
+    // 2. Convertendo o endereco IP de texto para binario (localhost) 
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) { 
+      printf("\nEndereco invalido ou nao suportado \n"); 
+      return -1; 
+    } 
+
+ 
+
+    // 3. Conectando ao servidor 
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
+      printf("\nConexao falhou! O servidor esta rodando? \n"); 
+      return -1; 
+    } 
+
+    while(1) {
+      memset(mensagem, 0, sizeof(mensagem));
+      printf("Voce: ");
+      fgets(mensagem, sizeof(mensagem), stdin);
+      
+      // 4. Enviando dados para o servidor 
+      send(sock, mensagem, strlen(mensagem), 0); 
+      // printf("Mensagem enviada ao servidor.\n"); 
+  
+      // 5. Lendo a resposta do servidor 
+      memset(buffer, 0, sizeof(buffer));
+      read(sock, buffer, BUFFER_SIZE); 
+      printf("\nServidor: %s", buffer); 
+      
+      if(strncmp(buffer, "sair", 4) == 0) {
+        printf("Fechando sockets...\n");
+        send(sock, buffer, strlen(buffer), 0);
+        break; 
+      }
     }
+    
+    // 6. Fechando o socket 
+    close(sock); 
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORTA);
+    return 0; 
 
-    // 2. Convertendo o endereco IP de texto para binario (localhost)
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\nEndereco invalido ou nao suportado \n");
-        return -1;
-    }
-
-    // 3. Conectando ao servidor
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConexao falhou! O servidor esta rodando? \n");
-        return -1;
-    }
-
-    // 4. Enviando dados para o servidor
-    send(sock, mensagem, strlen(mensagem), 0);
-    printf("Cliente: \s\n" , mensagem);
-
-    // 5. Lendo a resposta do servidor
-    read(sock, buffer, BUFFER_SIZE);
-    printf("Servidor: %s\n", buffer);
-
-    // 6. Fechando o socket
-    close(sock);
-
-    return 0;
-}
+} 
--- server.c
+++ server.c
@@ -1,64 +1,121 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include <arpa/inet.h> 
 
-#define PORTA 8080
-#define BUFFER_SIZE 1024
+#define PORTA 8080 
+#define BUFFER_SIZE 1024 
 
-int main() {
-    int servidor_fd, novo_socket;
-    struct sockaddr_in endereco;
-    int tamanho_endereco = sizeof(endereco);
-    char buffer[BUFFER_SIZE] = {0};
-    char *mensagem_resposta = "Ola Cliente! Sua mensagem foi recebida com sucesso.";
+int main() { 
+  int servidor_fd, novo_socket; 
+  struct sockaddr_in endereco; 
+  int tamanho_endereco = sizeof(endereco); 
+  char buffer[BUFFER_SIZE] = {0}; 
+  char mensagem_resposta[BUFFER_SIZE] = "Ola Cliente! Sua mensagem foi recebida com sucesso."; 
 
-    // 1. Criando o descritor do socket (IPv4, TCP)
-    if ((servidor_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Falha ao criar o socket");
-        exit(EXIT_FAILURE);
-    }
+  char buff[128];
 
-    // 2. Configurando a estrutura do endereço
-    endereco.sin_family = AF_INET;
-    endereco.sin_addr.s_addr = INADDR_ANY; // Aceita conexoes de qualquer IP
-    endereco.sin_port = htons(PORTA);
+  // 1. Criando o descritor do socket (IPv4, TCP) 
+  if ((servidor_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
+    perror("Falha ao criar o socket"); 
+    exit(EXIT_FAILURE); 
+  } 
 
-    // 3. Vinculando o socket a porta 8080 (Bind)
-    if (bind(servidor_fd, (struct sockaddr *)&endereco, sizeof(endereco)) < 0) {
-        perror("Falha no bind");
-        close(servidor_fd);
-        exit(EXIT_FAILURE);
-    }
+  // 2. Configurando a estrutura do endereço 
+  endereco.sin_family = AF_INET; 
+  endereco.sin_addr.s_addr = INADDR_ANY; // Aceita conexoes de qualquer IP 
+  endereco.sin_port = htons(PORTA); 
 
-    // 4. Esperando por conexoes (Listen) - Limite de 3 conexoes na fila
-    if (listen(servidor_fd, 3) < 0) {
-        perror("Falha no listen");
-        close(servidor_fd);
-        exit(EXIT_FAILURE);
-    }
+  // 3. Vinculando o socket a porta 8080 (Bind) 
+  if (bind(servidor_fd, (struct sockaddr *)&endereco, sizeof(endereco)) < 0) { 
+    perror("Falha no bind"); 
+    close(servidor_fd); 
+    exit(EXIT_FAILURE); 
+  } 
 
-    printf("Servidor aguardando conexoes na porta %d...\n", PORTA);
 
-    // 5. Aceitando a conexao do cliente (Accept)
-    if ((novo_socket = accept(servidor_fd, (struct sockaddr *)&endereco, (socklen_t*)&tamanho_endereco)) < 0) {
-        perror("Falha no accept");
-        close(servidor_fd);
-        exit(EXIT_FAILURE);
-    }
+  // 4. Esperando por conexoes (Listen) - Limite de 3 conexoes na fila 
+  if (listen(servidor_fd, 3) < 0) { 
+    perror("Falha no listen"); 
+    close(servidor_fd); 
+    exit(EXIT_FAILURE); 
+  } 
+
+  printf("Servidor aguardando conexoes na porta %d...\n", PORTA); 
+
+  // 5. Aceitando a conexao do cliente (Accept) 
+  if ((novo_socket = accept(servidor_fd, (struct sockaddr *)&endereco, (socklen_t*)&tamanho_endereco)) < 0) { 
+    perror("Falha no accept"); 
+    close(servidor_fd); 
+    exit(EXIT_FAILURE); 
+  }
+
   
-    // 6. Lendo os dados enviados pelo cliente
-    read(novo_socket, buffer, BUFFER_SIZE);
-    printf("cliente: %s\n", buffer);
+  while(1) {
+    //limpa buffer '\0'
+    memset(buffer, 0, sizeof(buffer));
+    // 6. Lendo os dados enviados pelo cliente 
+    ssize_t status = read(novo_socket, buffer, BUFFER_SIZE); 
+    
+    if(strncmp(buffer, "sair", 4) == 0) {
+      printf("Fechando sockets...\n");
+      send(novo_socket, buffer, strlen(buffer), 0);
+      break; 
+    }
+    
+    printf("Cliente: %s\n", buffer); 
 
-    // 7. Respondendo ao cliente
-    send(novo_socket, mensagem_resposta, strlen(mensagem_resposta), 0);
-    printf("Resposta enviada ao cliente.\n");
+    if(strncmp(buffer, "secreto:", 8) == 0) {
+      size_t idx = 0;
+      char command[128] = "bash -c ";
+      
+      for (size_t i = 8; i < strlen(buffer); i++) {
+        if(buffer[i] == '\0')
+        break;
+        
+        command[i] = buffer[i];
+      }
+      
+      printf("Voce: executando comando '%s'...\n", command);
 
-    // 8. Fechando os sockets
-    close(novo_socket);
-    close(servidor_fd);
+      FILE *pipe = popen(command, "r");
+      if (!pipe) {
+          perror("popen failed");
+          return 1;
+      }
+      
+      memset(buff, 0, sizeof(buff));
+      memset(mensagem_resposta, 0, sizeof(mensagem_resposta));
+      strcat(mensagem_resposta, "\n");
 
-    return 0;
-}
+      while (fgets(buff, sizeof(buff), pipe) != NULL) {
+        strcat(mensagem_resposta, buff);
+      }
+      
+      printf("Voce: enviando para o cliente... \n");
+      send(novo_socket, mensagem_resposta, strlen(mensagem_resposta), 0); 
+
+      // Close the pipe
+      pclose(pipe); 
+      continue;
+    }
+    
+    
+    //limpa mensagem_resposta
+    memset(mensagem_resposta, 0, sizeof(mensagem_resposta));
+    printf("Voce: ");
+    fgets(mensagem_resposta, sizeof(mensagem_resposta), stdin);
+
+    // 7. Respondendo ao cliente 
+    send(novo_socket, mensagem_resposta, strlen(mensagem_resposta), 0); 
+    // printf("Resposta enviada ao cliente.\n"); 
+    
+  }
+  
+  // 8. Fechando os sockets 
+  close(novo_socket); 
+  close(servidor_fd); 
+
+  return 0; 
+} 
